a82046392db646ac188f26f07cda722713d277f4
Ladrillo <gabriel@cabrejas.net>
2021-12-16 18:29:12 +0100

    committing to "lecture"

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,7 +1,10 @@
 const Hobbit = require('./hobbits-model')
 const db = require('../../data/dbConfig')
 
-beforeAll()
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
 
 beforeEach()
 
