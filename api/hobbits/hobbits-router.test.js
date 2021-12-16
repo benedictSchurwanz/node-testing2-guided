b9abad4cb50228ed03e67d940cf360aefcef13a8
Ladrillo <gabriel@cabrejas.net>
2021-12-16 19:21:22 +0100

    committing to "lecture"

--- api/hobbits/hobbits-router.test.js
+++ api/hobbits/hobbits-router.test.js
@@ -1,5 +1,16 @@
 // with supertest we always import the whole server
 // regardless of what router we are testing here specifically
-const server = require('../server')
 const request = require('supertest')
+const server = require('../server')
 const db = require('../../data/dbConfig')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+beforeEach(async () => {
+  await db.seed.run()
+})
+afterAll(async () => {
+  await db.destroy() // disconnects from db
+})
