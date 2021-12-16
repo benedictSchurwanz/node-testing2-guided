018b76f487d3391257e199480f7265c81ffee24e
Ladrillo <gabriel@cabrejas.net>
2021-12-16 19:20:51 +0100

    committing to "lecture"

--- api/hobbits/hobbits-router.test.js
+++ api/hobbits/hobbits-router.test.js
@@ -1,1 +1,5 @@
+// with supertest we always import the whole server
+// regardless of what router we are testing here specifically
 const server = require('../server')
+const request = require('supertest')
+const db = require('../../data/dbConfig')
